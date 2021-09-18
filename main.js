035e784757afebfa220707801ff64c15082aa480
SimoneBal <80442925+SimoneBal@users.noreply.github.com>
2021-09-18 11:37:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jumpie=loadSound("jump.wav");
+	mario_coinie=loadSound("coin.wav");
+	mario_dead=loadSound("gameover.wav");
+	mario_gumbo=loadSound("kick.wav");
+	mario_died=loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent('canvas')
+
+	video=createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+	game()
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
+  
+  function gotPoses(results)
+  {
+	if(results.length > 0)
+	{
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
+	  console.log(results)
+	}
+  }
+  
+
+
+
+
+
